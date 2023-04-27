2624e7579a70ffea47a4063d9baa9f59b3a6a2d0
mouayad03 <116551140+mouayad03@users.noreply.github.com>
2023-04-27 20:31:15 +0200

    ✅list, create, update and delete content

--- server/api/api.ts
+++ server/api/api.ts
@@ -20,40 +20,45 @@ export class API {
     this.app.get('/hello', this.sayHello.bind(this));
     this.app.post('/register', this.register.bind(this));
     this.app.post('/login', this.login.bind(this));
+    this.app.get('/tweets', this.getTweets.bind(this));
+    this.app.post('/tweet', this.postTweet.bind(this));
+    this.app.put('/tweet/:id', this.updateTweet.bind(this));
+    this.app.delete('/tweet/:id', this.deleteTweet.bind(this));
+   //this.app.get('/whoAmI', this.whoAmI.bind(this));
   }
   // Methods
   private async sayHello(req: Request, res: Response) {
     const result = await db.executeSQL('SELECT * FROM users');
-    
+
     res.send(result);
   }
 
   private async register(req: Request, res: Response) {
     const { username, password, role } = req.body;
     const hashedPassword = crypto.createHash('sha256').update(password).digest('hex');
-  
+
     const existingUser = await db.executeSQL('SELECT name FROM users WHERE name = ?', [username]);
     if (existingUser.length > 0) {
       return res.status(400).send("Username already exists. Please choose a different username.");
     }
-  
+
     const userRole = role || "C";
-  
+
     try {
       await db.executeSQL(
         `INSERT INTO users (name, password, role) VALUES (?, ?, ?)`,
         [username, hashedPassword, userRole]
-      );          
+      );
       res.send(`User ${username} registered successfully.`);
     } catch (error) {
       console.log(error);
       res.status(500).send('Failed to register user.');
     }
-  }  
-  
+  }
+
   private async login(req: Request, res: Response) {
     const { username, password } = req.body;
-  
+
     try {
       const result = await db.executeSQL('SELECT name, password FROM users WHERE name = ?', [username]);
       if (result.length > 0) {
@@ -70,12 +75,59 @@ export class API {
         res.send('Invalid username or password.');
       }
     } catch (error) {
-      console.log(error);
       res.status(500).send('Failed to login.');
     }
   }
-  
+
   private generateAccessToken(username: { username: string }) {
     return jwt.sign(username, TOKEN_SECRET, { expiresIn: '1800s' });
+  }
+
+  private async getTweets(req: Request, res: Response) {
+    const result = await db.executeSQL('SELECT content FROM tweets');
+    if (result.length === 0) {
+      res.status(200).send("There are no tweets yet");
+    } else {
+      res.status(200).send(result);
+    }
+  }
+
+  private async postTweet(req: Request, res: Response) {
+    const { user_id, content } = req.body;
+
+    try {
+      await db.executeSQL(
+        `INSERT INTO tweets (user_id, content) VALUES (?, ?)`,
+        [user_id, content]
+      );
+      res.send(`Thanks for your post.`);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send('Failed to post content.');
+    }
+  }
+
+  private async updateTweet(req: Request, res: Response) {
+    const id = req.params.id;
+    const { user_id, content } = req.body;
+  
+    const result = await db.executeSQL('UPDATE tweets SET `user_id`= ?, `content`= ? WHERE id = ?', [user_id, content, id]);
+  
+    if (result.affectedRows === 0) {
+      res.status(400).send("There are no tweets to update");
+    } else {
+      res.status(200).send(`The Tweet has been updated`);
+    }
+  } 
+
+  private async deleteTweet(req: Request, res: Response) {
+    const id = req.params.id;
+
+    const result = await db.executeSQL('DELETE FROM tweets WHERE id = ?', [id]);
+    if (result.affectedRows === 0) {
+      res.status(400).send("There are no tweets to delete");
+    } else {
+      res.status(200).send(`The Tweet has been deleted`);
+    }
   }
 }
