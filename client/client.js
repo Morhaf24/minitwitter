4e7eaf5473940f2f779eb826b5ba6116bd871662
DESKTOP-CA512TH\Morha <morhaf.alnhlawe@gmail.com>
2023-04-28 13:25:16 +0200

    ✅ Login

--- client/client.js
+++ client/client.js
@@ -2,10 +2,41 @@ if (location.host.includes('localhost')) {
   // Load livereload script if we are on localhost
   document.write(
     '<script src="http://' +
-      (location.host || 'localhost').split(':')[0] +
-      ':35729/livereload.js?snipver=1"></' +
-      'script>'
+    (location.host || 'localhost').split(':')[0] +
+    ':35729/livereload.js?snipver=1"></' +
+    'script>'
   )
 }
 
-console.log('This is a Test')
+const loginForm = document.getElementById("login-form");
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+let loginRequest;
+
+loginForm.addEventListener("submit", onLoginFormSubmitted);
+
+function onLoginFormSubmitted(event) {
+  event.preventDefault();
+
+  const loginData = {
+    username: usernameInput.value,
+    password: passwordInput.value
+  };
+
+  loginRequest = new XMLHttpRequest();
+  loginRequest.open("POST", "http://localhost:4200/login");
+  loginRequest.setRequestHeader("Content-Type", "application/json");
+  loginRequest.onreadystatechange = onLoginResponse;
+  loginRequest.send(JSON.stringify(loginData));
+}
+
+function onLoginResponse() {
+  if (loginRequest.readyState < 4) {
+    const responseStatus = loginRequest.status;
+    if (responseStatus === 200) {
+      alert("Success");
+    } else {
+      alert("Login failed");
+    }
+  }
+}
