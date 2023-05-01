fb6939d085314507ed190ea798da9badcb9dd6f4
mouayad03 <116551140+mouayad03@users.noreply.github.com>
2023-05-01 11:27:11 +0200

    ✅Database scheme

--- server/database/database.ts
+++ server/database/database.ts
@@ -1,6 +1,6 @@
 import mariadb from 'mariadb'
 import { Pool } from 'mariadb'
-import { COMMENT_TABLES, LIKES_TABLE, TWEETS_TABLE, USERS_TABLE, INSERT, ALTER_COMMENTS, ALTER_LIKES, ALTER_TWEETS, ALTER_USERS, MODIFY_COMMENT, MODIFY_LIKES, MODIFY_TWEETS, MODIFY_USERS, CONSTRAINT_COMMENT, CONSTRAINT_LIKES, CONSTRAINT_TWEETS } from './schema'
+import { COMMENT_TABLES, LIKES_TABLE, TWEETS_TABLE, USERS_TABLE, INSERT } from './schema'
 
 export class Database {
   // Properties
@@ -19,22 +19,11 @@ export class Database {
   // Methods
   private initializeDBSchema = async () => {
     console.log('Initializing DB schema...')
-    await this.executeSQL(COMMENT_TABLES)
-    await this.executeSQL(LIKES_TABLE)
-    await this.executeSQL(TWEETS_TABLE)
-    await this.executeSQL(USERS_TABLE)
-    await this.executeSQL(INSERT)
-    await this.executeSQL(ALTER_COMMENTS)
-    await this.executeSQL(ALTER_LIKES)
-    await this.executeSQL(ALTER_TWEETS)
-    await this.executeSQL(ALTER_USERS)
-    await this.executeSQL(MODIFY_COMMENT)
-    await this.executeSQL(MODIFY_LIKES)
-    await this.executeSQL(MODIFY_TWEETS)
-    await this.executeSQL(MODIFY_USERS)
-    await this.executeSQL(CONSTRAINT_COMMENT)
-    await this.executeSQL(CONSTRAINT_LIKES)
-    await this.executeSQL(CONSTRAINT_TWEETS)
+    await this.executeSQL(COMMENT_TABLES);
+    await this.executeSQL(LIKES_TABLE);
+    await this.executeSQL(TWEETS_TABLE);
+    await this.executeSQL(USERS_TABLE);
+    await this.executeSQL(INSERT);
   }
 
   public executeSQL = async (query: string, values?: any[]) => {
