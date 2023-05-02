fcea45abd0300bc90ca2a3a45af3ff10bf3a2685
DESKTOP-CA512TH\Morha <morhaf.alnhlawe@gmail.com>
2023-05-02 08:24:46 +0200

    ✅ Post the Posts

--- client/post.js
+++ client/post.js
@@ -1,40 +1,40 @@
 if (location.host.includes('localhost')) {
-    // Load livereload script if we are on localhost
-    document.write(
-      '<script src="http://' +
-      (location.host || 'localhost').split(':')[0] +
-      ':35729/livereload.js?snipver=1"></' +
-      'script>'
-    )
-  }
+  // Load livereload script if we are on localhost
+  document.write(
+    '<script src="http://' +
+    (location.host || 'localhost').split(':')[0] +
+    ':35729/livereload.js?snipver=1"></' +
+    'script>'
+  )
+}
+
+let postForm = document.getElementById("create_post");
+const contentInput = document.getElementById("post_input");
+let postRequest;
+
+postForm.addEventListener("submit", onPostFormSubmitted);
+
+function onPostFormSubmitted(event) {
+  event.preventDefault();
+
+  const postData = {
+    content: postInput.value
+  };
   
-  let postForm = document.getElementById("create_post");
-  const post_Input = document.getElementById("post_input");
-  let postRequest;
-  
-  postForm.addEventListener("submit", onPostFormSubmitted);
-  
-  function onPostFormSubmitted(event) {
-    event.preventDefault();
-  
-    const postData = {
-        content: post_Input.value
-    };
-  
-    postRequest = new XMLHttpRequest();
-    postRequest.open("POST", "http://localhost:4200/tweet");
-    postRequest.setRequestHeader("Content-Type", "application/json");
-    postRequest.onreadystatechange = onPostResponse;
-    postRequest.send(JSON.stringify(postData));
-  }
-  
-  function onPostResponse() {
-    if (postRequest.readyState < 4) {
-      const responseStatus = postRequest.status;
-      if (responseStatus === 200) {
-        alert("Success");
-      } else {
-        alert("Login failed");
-      }
+  postRequest = new XMLHttpRequest();
+  postRequest.open("POST", "http://localhost:4200/tweet");
+  postRequest.setRequestHeader("Content-Type", "application/json");
+  postRequest.onreadystatechange = onPostResponse;
+  postRequest.send(JSON.stringify(postData));
+}
+
+function onPostResponse() {
+  if (postRequest.readyState < 4) {
+    const responseStatus = postRequest.status;
+    if (responseStatus === 200) {
+      window.location.reload();
+    } else {
+      alert("Login failed");
     }
   }
+}
